90a5991a04126a43d4973c96ae2e61adf825ee06
dhaval dhanani <dhaval070@gmail.com>
2024-09-28 19:38:29 +0530

    allow selecting surfaces

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,58 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
+import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import PropTypes from 'prop-types';
+import { Field, Label, Select, Button } from '@headlessui/react'
 
+var apiurl = "http://localhost:8000"
 // axios.defaults.headers["Origin"] = "localhost"
 function App() {
   const [siteLoc, setSiteLoc] = useState([])
   const [site, setSite] = useState("")
   const [allSites, setAllSites] = useState([])
+  const [isOpen, setIsOpen] = useState(false)
+  const [currSiteLoc, setCurrSiteLoc] = useState(null)
+
+  SurfaceDialog.propTypes = {
+      isOpen: PropTypes.bool.isRequired,
+      setIsOpen: PropTypes.func,
+      surfaceSelected: PropTypes.func,
+      siteLoc: PropTypes.object,
+  }
 
   useEffect(function() {
       if (site == "") {
           return
       }
       console.log("site", site)
-      axios.get("http://localhost:8000/site-locations/"+ site.toString()).then((resp) => {
+      axios.get(apiurl + "/site-locations/"+ site.toString()).then((resp) => {
         setSiteLoc(resp.data)
       }).catch(e => console.error(e))
   },[site]);
 
   useEffect(function() {
-    axios.get("http://localhost:8000/sites").then((resp) => {
+    axios.get(apiurl + "/sites").then((resp) => {
         setAllSites(resp.data)
     }).catch(e => console.error(e))
   },[])
+
+
+  let assignSurface = function(rec) {
+      setCurrSiteLoc(rec)
+      setIsOpen(true)
+  }
+
+  let surfaceSelected = function(id, siteloc) {
+      console.log(siteloc)
+    axios.post(apiurl + "/set-surface", {
+        site: site,
+        location: siteloc.location,
+        surface_id: id,
+    }).then(resp => setSiteLoc(resp.data)).catch((e) => console.error(e)).finally(() => {
+        setIsOpen(false)
+    })
+  }
 
     let rows = []
 
@@ -31,10 +61,14 @@ function App() {
             <tr key={r.location}>
               <td>{r.location}</td>
               <td>{r.address}</td>
+              <td>{r.surface_id}</td>
               <td>
-                  {r.surface_id}
+                <Button className="rounded bg-sky-600 py-2 px-2 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700" onClick={() => assignSurface(r)}>Change</Button>
+                &nbsp;&nbsp;
+                { r.surface_id != 0 &&
+                <Button className="rounded bg-sky-600 py-2 px-2 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700" onClick={() => surfaceSelected(0, r)}>Unset</Button>
+}
               </td>
-              <td><a href="#" >Set</a></td>
             </tr>
         ))
     }
@@ -45,26 +79,87 @@ function App() {
             <option key={r.site}>{r.site}</option>
         ))
     }
+
   return (
-    <>
-      <h1>Match Surfaces</h1>
-      <div className="card">
-      Site
-          <select onChange={(e) => setSite(e.currentTarget.value)}>
+    <div className="App w-max">
+    <SurfaceDialog isOpen={isOpen} siteLoc={currSiteLoc} setIsOpen={setIsOpen} surfaceSelected={surfaceSelected} />
+      <h1 className="text-3xl font-bold text-center">Match Surfaces</h1>
+      <div className="">
+    <Field>
+      <Label  className="text-sm/6 font-medium ">Site</Label>
+      <div className="relative">
+          <Select onChange={(e) => setSite(e.currentTarget.value)}>
             <option value="">Select</option>
             {options}
-          </select>
-      <table width="100%">
+          </Select>
+
+      </div>
+        </Field>
+      <table className="table-auto">
       <tbody>
         <tr>
-            <th>Location</th><th>Address</th><th>Surface</th><th></th>
+            <th>Location</th><th>Address</th><th>Surface ID</th><th></th>
         </tr>
         {rows}
         </tbody>
       </table>
       </div>
-    </>
+    </div>
   )
 }
 
+function SurfaceDialog(props) {
+    const [surfaces, setSurfaces] = useState([])
+
+    useEffect(function() {
+        axios.get(apiurl + "/surfaces").then((res) => {
+            setSurfaces(res.data)
+        }).catch(e => console.error(e))
+    },[])
+
+    let res = []
+    if (surfaces.length > 0) {
+        res = surfaces.map(r => (
+            <tr key={r.id}>
+                <td><a href="#" className="font-bold text-blue-900" onClick={() => props.surfaceSelected(r.id, props.siteLoc)} >{r.id}</a></td>
+                <td>{r.name}</td>
+                <td>{r.Location.name}</td>
+                <td>{r.Location.address1}</td>
+                <td>{r.Location.city}</td>
+            </tr>
+        ))
+    }
+    return (
+        <>
+            <Dialog open={props.isOpen} onClose={() => props.setIsOpen(false)} className="relative z-50">
+                <div className="fixed inset-0 flex w-screen justify-center bg-white  p-4">
+                  <div className="flex items-center justify-center ">
+                      <DialogPanel className=" w-max  h-full overflow-auto  space-y-2 border bg-white p-2">
+                        <DialogTitle className="font-bold">
+                            Select surface for <span className="text-orange-500">{props.siteLoc && props.siteLoc.location}</span>
+                        </DialogTitle>
+                        <Description></Description>
+
+                        <table className="table-auto w-full ">
+                            <thead className="sticky top-0">
+                                <tr className="bg-gray-100">
+                                    <th>ID</th><th>Name</th><th>Location</th><th>Address1</th><th>City</th>
+                                </tr>
+                            </thead>
+                            <tbody className="overflow-y-auto">
+                                {res}
+                            </tbody>
+                        </table>
+                        <p>
+                        </p>
+                        <div className="flex gap-4">
+                          <Button className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700" onClick={() => props.setIsOpen(false)}>Cancel</Button>
+                        </div>
+                      </DialogPanel>
+                  </div>
+                </div>
+              </Dialog>
+        </>
+    )
+}
 export default App
