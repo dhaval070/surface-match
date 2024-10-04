95516aac9333fd2d687c14adf7a741eed2133eab
dhaval dhanani <dhaval070@gmail.com>
2024-10-05 01:54:31 +0530

    added surface report

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -10,14 +10,25 @@ export default function Layout() {
         auth.logOut()
     }
 
+    const apiurl = import.meta.env.VITE_API_URL
+    const report = () => {
+        window.open(apiurl + "/report")
+
+    }
     return <div>
-    <div className="text-right">
     {auth.user &&
         <>
-        Hi, {auth.user} &nbsp;
-        <Button onClick={logout} className="rounded bg-amber-600 py-2 px-4 text-sm text-white data-[hover]:bg-amber-500 data-[active]:bg-sky-700">Logout</Button>
-        </>}
+    <div className="flex" >
+        <div className="text-left grow">
+            <button className="rounded text-left bg-slate-400 py-2 px-4 text-sm  data-[hover]:bg-slate-500 data-[active]:bg-sky-700" onClick={report}>Surface Report</button>
+        </div>
+        <div className="justify-right">
+            Hi, {auth.user} &nbsp;
+            <Button onClick={logout} className="rounded bg-amber-600 py-2 px-4 text-sm text-white data-[hover]:bg-amber-500 data-[active]:bg-sky-700">Logout</Button>
+        </div>
     </div>
+        </>
+    }
     <Outlet />
     </div>
 
